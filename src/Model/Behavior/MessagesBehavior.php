6083a972b1e56af9a9dbc0c9bb1638ba44fb4d01
delAmux <luisdelamo@gmail.com>
2017-06-21 00:39:15 +0100

    add behavior

---
+++ src/Model/Behavior/MessagesBehavior.php
@@ -0,0 +1,37 @@
+<?php
+namespace CakeMojo\Messages\Model\Behavior;
+
+use Cake\Datasource\RepositoryInterface;
+use Cake\ORM\Behavior;
+use Cake\ORM\Table;
+use Cake\ORM\TableRegistry;
+
+/**
+ * MessagesBehavior behavior
+ */
+class MessagesBehavior extends Behavior
+{
+
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+
+    ];
+
+    public function addMessage($id, array $data){
+        $messagesTable = TableRegistry::get('Messages');
+
+        $message = $messagesTable->newEntity();
+
+        $message->id = $id;
+        $message->subject = $data['subject'];
+        $message->body = $data['body'];
+        $message->model = empty($data['model']) ? RepositoryInterface::alias() : $data['model'];
+
+        $messagesTable->save($message);
+
+    }
+}
