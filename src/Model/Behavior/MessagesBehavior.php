26564830be48f0616c2038d3ec703d9bf95b052b
delAmux <luisdelamo@gmail.com>
2017-09-19 01:53:54 +0100

    se documenta la funciion y se añade configuracion inicial

--- src/Model/Behavior/MessagesBehavior.php
+++ src/Model/Behavior/MessagesBehavior.php
@@ -1,10 +1,10 @@
 <?php
 namespace CakeMojo\Messages\Model\Behavior;
 
-use Cake\Datasource\RepositoryInterface;
 use Cake\ORM\Behavior;
 use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
+use Cake\Utility\Hash;
 
 /**
  * MessagesBehavior behavior
@@ -14,14 +14,22 @@ class MessagesBehavior extends Behavior
 
     /**
      * Default configuration.
-     *
+     * @var id //of the table for pass like foreign key
+     * @array data
+     * -subject
+     * -body
+     * -model //you can to assign table name or get automatically from model
      * @var array
      */
     protected $_defaultConfig = [
-
+        'subject' => 'Message subject',
+        'body' => 'This is the body'
     ];
 
     public function addMessage($id, array $data){
+        $config = $this->_defaultConfig;
+        Hash::merge($config, $data);
+
         $messagesTable = TableRegistry::get('CakeMojo/Messages.Messages');
 
         $message = $messagesTable->newEntity();
